d6a8629dcc92cfc63e7aee1a591231349db5cda6
muhammad.umair@intrasols.com <muhammad.umair@intrasols.com>
2020-03-27 21:09:52 +0500

    Initial Commit

---
+++ DattingApiCore/DattingApiCore/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DattingApiCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DattingApiCore.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
